f830b1510c4af51c6f9d70e5031cd31d15e031e3
Lrfoster03 <lrfoster03@gmail.com>
2025-09-22 20:52:23 -0700

    bugfix: netlify with vite and subdirectories

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-  base: '/bounce/',
-})
+export default defineConfig(({ mode }) => {
+  let base = "/"; // default for PR deploys
+  if (mode === "production") {
+    base = "/bounce/"; // your real production path
+  }
+
+  return {
+    plugins: [react()],
+    base,
+  };
+});
