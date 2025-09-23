05de0959e883e12b1dd4c35626e0003fbb0f450e
Lrfoster03 <lrfoster03@gmail.com>
2025-09-22 20:55:57 -0700

    bugfix: netlify with vite?

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ mode }) => {
-  let base = "/"; // default for PR deploys
-  if (mode === "production") {
-    base = "/bounce/"; // your real production path
-  }
+  const isPR = process.env.NETLIFY && process.env.CONTEXT !== "production";
+  const base = isPR ? "/" : "/bounce/"; // root for PRs, subpath for production
 
   return {
     plugins: [react()],
